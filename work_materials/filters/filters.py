7ef5ed9f4a99d4ff26c33b4d82ed540cd1dc8f75
Cactiw <knyazev1514@gmail.com>
2019-02-14 15:41:46 +0300

    Фикс некоторых ошибок в фильтрах

--- work_materials/filters/filters.py
+++ work_materials/filters/filters.py
@@ -29,22 +29,22 @@ class Filter_Instant_Report(BaseFilter):
 class Filter_Castle(BaseFilter):
     def filter(self, message):
         if message.text:
-            return message.text in castles and dispatcher.user_data.get(message.from_user.id).get("status") == "start"
+            user_data = dispatcher.user_data.get(message.from_user.id)
+            return message.text in castles and user_data and user_data.get("status") == "start"
 
 class Filter_Results(BaseFilter):
     def filter(self, message):
         if message.text:
             if message.forward_from_chat is not None:
                 print(message.forward_from)
-                return message.forward_from_chat.id == -1001391784649 and dispatcher.user_data.get(message.from_user.id).get("status") == "selected_castle"
+                user_data = dispatcher.user_data.get(message.from_user.id)
+                return message.forward_from_chat.id == -1001391784649 and user_data and user_data.get("status") == "selected_castle"
             return 0
 
 class Filter_Report(BaseFilter):
     def filter(self, message):
         if message.text:
             if dispatcher.user_data.get(message.from_user.id) is None:
-                print("User_data is None, id =", message.from_user.id)
-                print(message.text)
                 dispatcher.bot.send_message(chat_id=message.from_user.id, text="Произошла ошибка, попробуйте нажать /start")
                 return False
             return dispatcher.user_data.get(message.from_user.id).get("status") == "results" and message.text.find("Твои результаты в бою:") != -1
@@ -52,11 +52,12 @@ class Filter_Report(BaseFilter):
 class FilterDDGReport(BaseFilter):
     def filter(self, message):
         if message.text:
-            if dispatcher.user_data.get(message.from_user.id) is None:
+            user_data = dispatcher.user_data.get(message.from_user.id)
+            if user_data is None:
                 print("User_data is None, id =", message.from_user.id)
                 print(message.text)
                 dispatcher.bot.send_message(chat_id=message.from_user.id, text="Произошла ошибка, попробуйте нажать /start")
-            return dispatcher.user_data.get(message.from_user.id).get("status") == "results" and message.text.find("DDG:") == 0
+            return user_data.get("status") == "results" and message.text.find("DDG:") == 0
 
 
 
